3ee03bcd47bb6edc6adb9990890d2f9d0cb8f29f
Ruby <ruby@ubuntu.com>
2019-06-21 15:37:24 -0400

    added annotate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
   has_many :comments, dependent: :delete_all
 end
